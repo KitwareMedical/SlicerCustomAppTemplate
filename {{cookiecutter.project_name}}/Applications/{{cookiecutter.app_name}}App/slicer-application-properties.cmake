KitwareMedical/SlicerCustomAppTemplate

commit bc64b80e3302281c208c217e5d47b90a7cf6b7d3

ENH: Install License Default

{{cookiecutter.project_name}}/Applications/{{cookiecutter.app_name}}App/slicer-application-properties.cmake
@@ -30,9 +30,6 @@ set(WIN_ICON_FILE
   "${CMAKE_CURRENT_LIST_DIR}/Resources/Icons/DesktopIcon.ico"
   )
 
-set(PROJECT_SOURCE_DIR
-  "{{cookiecutter.projection_name}}_SOURCE_DIR"
-  )
 set(LICENSE_FILE
-  "${PROJECT_SOURCE_DIR}/License.txt"
+  "${ '{' }}{{cookiecutter.project_name}}_SOURCE_DIR}/LICENSE"
   )

commit 4ba39c621706b5096f42b5aaef7d69268c2f5e30

COMP: Fix configuration of license default
See https://cookiecutter.readthedocs.io/en/1.7.0/troubleshooting.html

This commit fixes the following error introduced in bc64b80 (ENH: Install License
Default):

  CMake Error at /path/to/SlicerCustomAppTemplateTest/Awesome/Applications/AwesomeApp/slicer-application-properties.cmake:33 (set):
    Syntax error in cmake code at

      /path/to/SlicerCustomAppTemplateTest/Awesome/Applications/AwesomeApp/slicer-application-properties.cmake:34

    when parsing string

      ${ '{' } Awesome_SOURCE_DIR}/LICENSE

    Invalid character (' ') in a variable name: ''

{{cookiecutter.project_name}}/Applications/{{cookiecutter.app_name}}App/slicer-application-properties.cmake
@@ -31,5 +31,5 @@ set(WIN_ICON_FILE
   )
 
 set(LICENSE_FILE
-  "${ '{' }}{{cookiecutter.project_name}}_SOURCE_DIR}/LICENSE"
+  "${{ "{" }}{{cookiecutter.project_name}}_SOURCE_DIR}/LICENSE"
   )
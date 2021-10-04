9f0185208d606f66ae86910b8d2e7bfe1f97ac43
Connor Bowley <connor.bowley@kitware.com>
2021-10-04 11:34:59 -0400

    ENH: Update SlicerCAT to work with new Slicer logo changes

--- {{cookiecutter.project_name}}/Applications/{{cookiecutter.app_name}}App/q{{cookiecutter.app_name}}AppMainWindow.cxx
+++ {{cookiecutter.project_name}}/Applications/{{cookiecutter.app_name}}App/q{{cookiecutter.app_name}}AppMainWindow.cxx
@@ -21,6 +21,7 @@
 
 // Qt includes
 #include <QDesktopWidget>
+#include <QLabel>
 
 // Slicer includes
 #include "qSlicerApplication.h"
@@ -77,8 +78,19 @@ void q{{cookiecutter.app_name}}AppMainWindowPrivate::setupUi(QMainWindow * mainW
   //----------------------------------------------------------------------------
   mainWindow->setWindowIcon(QIcon(":/Icons/Medium/DesktopIcon.png"));
 
-  QPixmap logo(":/LogoFull.png");
-  this->LogoLabel->setPixmap(logo);
+  QLabel* logoLabel = new QLabel();
+  logoLabel->setObjectName("LogoLabel");
+  // QIcon stores multiple versions of the image (in different sizes)
+  // and uses the most suitable one (depending on DevicePixelRatio).
+  // QLabel cannot take a QIcon, therefore we need to get the most suitable
+  // QPixmap from the QIcon (base.png, base@2x, ...).
+  // To achieve this, we first determine the pixmap size in device independent units,
+  // which is the size of the base image (icon.availableSizes().first(), because for that
+  // DevicePixelRatio=1.0), and then we retieve the pixmap for this size.
+  QIcon icon = QIcon(":/LogoFull.png");
+  QPixmap logo = icon.pixmap(icon.availableSizes().first());
+  logoLabel->setPixmap(logo);
+  this->PanelDockWidget->setTitleBarWidget(logoLabel);
 
   // Hide the toolbars
   this->MainToolBar->setVisible(false);

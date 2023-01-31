35a1bde0d08da57cd0c6dcc3e7b728544186b77d
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-01-31 18:15:55 -0500

    STYLE: Update python code with indentation fixes
    Adapted from Slicer/Slicer@9390be6bf
    
    This fixes all remaining pycodestyle E1 indentation error codes.
    
    Bulk of updates performed using autopep8 CLI
    with the following command:
    
      autopep8 --in-place --select E111,E114,E115,E116,E117,E121,E122,E123,E124,E125,E126,E127,E128,E129,E131 $(git ls-files '*.py')
    
    See https://github.com/hhatto/autopep8#readme
    
    References:
    
    * Indentation is not a multiple of four
      See https://www.flake8rules.com/rules/E111.html
    
    * Indentation is not a multiple of four (comment)
      See https://www.flake8rules.com/rules/E114.html
    
    * Expected an indented block (comment)
      See https://www.flake8rules.com/rules/E115.html
    
    * Unexpected indentation (comment)
      See https://www.flake8rules.com/rules/E116.html
    
    * Over-indented
      See https://www.flake8rules.com/rules/E117.html
    
    * Continuation line under-indented for hanging indent
      See https://www.flake8rules.com/rules/E121.html
    
    * Continuation line missing indentation or outdented
      See https://www.flake8rules.com/rules/E122.html
    
    * Closing bracket does not match indentation of opening bracket's line
      See https://www.flake8rules.com/rules/E123.html
    
    * Closing bracket does not match visual indentation
      See https://www.flake8rules.com/rules/E124.html
    
    * Continuation line with same indent as next logical line
      See https://www.flake8rules.com/rules/E125.html
    
    * Continuation line over-indented for hanging indent
      See https://www.flake8rules.com/rules/E126.html
    
    * Continuation line over-indented for visual indent
      See https://www.flake8rules.com/rules/E127.html
    
    * Continuation line under-indented for visual indent
      See https://www.flake8rules.com/rules/E128.html
    
    * Visually indented line with same indent as next logical line
      See https://www.flake8rules.com/rules/E129.html
    
    * Continuation line unaligned for hanging indent
      See https://www.flake8rules.com/rules/E131.html
    
    Co-authored-by: James Butler <jamesobutler@users.noreply.github.com>

--- {{cookiecutter.project_name}}/Modules/Scripted/Home/Home.py
+++ {{cookiecutter.project_name}}/Modules/Scripted/Home/Home.py
@@ -4,167 +4,167 @@ import qt
 
 import slicer
 from slicer.ScriptedLoadableModule import (
-  ScriptedLoadableModule,
-  ScriptedLoadableModuleLogic,
-  ScriptedLoadableModuleWidget,
+    ScriptedLoadableModule,
+    ScriptedLoadableModuleLogic,
+    ScriptedLoadableModuleWidget,
 )
 from slicer.util import VTKObservationMixin
 
 
 class Home(ScriptedLoadableModule):
-  """The home module allows to orchestrate and style the overall application workflow.
+    """The home module allows to orchestrate and style the overall application workflow.
 
-  It is a "special" module in the sense that its role is to customize the application and
-  coordinate a workflow between other "regular" modules.
+    It is a "special" module in the sense that its role is to customize the application and
+    coordinate a workflow between other "regular" modules.
 
-  Associated widget and logic are not intended to be initialized multiple times.
-  """
+    Associated widget and logic are not intended to be initialized multiple times.
+    """
 
-  def __init__(self, parent):
-    ScriptedLoadableModule.__init__(self, parent)
-    self.parent.title = "Home"
-    self.parent.categories = [""]
-    self.parent.dependencies = []
-    self.parent.contributors = ["Sam Horvath (Kitware Inc.)", "Jean-Christophe Fillion-Robin (Kitware Inc.)"]
-    self.parent.helpText = """This module orchestrates and styles the overall application workflow."""
-    self.parent.helpText += self.getDefaultModuleDocumentationLink()
-    self.parent.acknowledgementText = """..."""  # replace with organization, grant and thanks.
+    def __init__(self, parent):
+        ScriptedLoadableModule.__init__(self, parent)
+        self.parent.title = "Home"
+        self.parent.categories = [""]
+        self.parent.dependencies = []
+        self.parent.contributors = ["Sam Horvath (Kitware Inc.)", "Jean-Christophe Fillion-Robin (Kitware Inc.)"]
+        self.parent.helpText = """This module orchestrates and styles the overall application workflow."""
+        self.parent.helpText += self.getDefaultModuleDocumentationLink()
+        self.parent.acknowledgementText = """..."""  # replace with organization, grant and thanks.
 
 
 class HomeWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
-  """Uses ScriptedLoadableModuleWidget base class, available at:
-  https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
-  """
+    """Uses ScriptedLoadableModuleWidget base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
 
-  def __init__(self, parent):
-    ScriptedLoadableModuleWidget.__init__(self, parent)
-    VTKObservationMixin.__init__(self)
+    def __init__(self, parent):
+        ScriptedLoadableModuleWidget.__init__(self, parent)
+        VTKObservationMixin.__init__(self)
 
-  def setup(self):
-    ScriptedLoadableModuleWidget.setup(self)
+    def setup(self):
+        ScriptedLoadableModuleWidget.setup(self)
 
-    # Load widget from .ui file (created by Qt Designer)
-    self.uiWidget = slicer.util.loadUI(self.resourcePath("UI/Home.ui"))
-    self.layout.addWidget(self.uiWidget)
-    self.ui = slicer.util.childWidgetVariables(self.uiWidget)
+        # Load widget from .ui file (created by Qt Designer)
+        self.uiWidget = slicer.util.loadUI(self.resourcePath("UI/Home.ui"))
+        self.layout.addWidget(self.uiWidget)
+        self.ui = slicer.util.childWidgetVariables(self.uiWidget)
 
-    # Remove unneeded UI elements
-    self.modifyWindowUI()
+        # Remove unneeded UI elements
+        self.modifyWindowUI()
 
-    # Create logic class
-    self.logic = HomeLogic()
+        # Create logic class
+        self.logic = HomeLogic()
 
-    # Setup scene defaults
-    self.setupNodes()
+        # Setup scene defaults
+        self.setupNodes()
 
-    # Dark palette does not propagate on its own?
-    self.uiWidget.setPalette(slicer.util.mainWindow().style().standardPalette())
+        # Dark palette does not propagate on its own?
+        self.uiWidget.setPalette(slicer.util.mainWindow().style().standardPalette())
 
-    self.setCustomUIVisible(True)
+        self.setCustomUIVisible(True)
 
-    # Apply style
-    self.applyApplicationStyle()
+        # Apply style
+        self.applyApplicationStyle()
 
-  def setupNodes(self):
-    self.logic.setup3DView()
-    self.logic.setupSliceViewers()
+    def setupNodes(self):
+        self.logic.setup3DView()
+        self.logic.setupSliceViewers()
 
-  def onClose(self, unusedOne, unusedTwo):
-    pass
+    def onClose(self, unusedOne, unusedTwo):
+        pass
 
-  def cleanup(self):
-    pass
+    def cleanup(self):
+        pass
 
-  def setSlicerUIVisible(self, visible):
-    slicer.util.setDataProbeVisible(visible)
-    slicer.util.setMenuBarsVisible(visible, ignore=["MainToolBar", "ViewToolBar"])
-    slicer.util.setModuleHelpSectionVisible(visible)
-    slicer.util.setModulePanelTitleVisible(visible)
-    slicer.util.setPythonConsoleVisible(visible)
-    slicer.util.setApplicationLogoVisible(visible)
-    keepToolbars = [
-      slicer.util.findChild(slicer.util.mainWindow(), "MainToolBar"),
-      slicer.util.findChild(slicer.util.mainWindow(), "ViewToolBar"),
-      slicer.util.findChild(slicer.util.mainWindow(), "CustomToolBar"),
-      ]
-    slicer.util.setToolbarsVisible(visible, keepToolbars)
+    def setSlicerUIVisible(self, visible):
+        slicer.util.setDataProbeVisible(visible)
+        slicer.util.setMenuBarsVisible(visible, ignore=["MainToolBar", "ViewToolBar"])
+        slicer.util.setModuleHelpSectionVisible(visible)
+        slicer.util.setModulePanelTitleVisible(visible)
+        slicer.util.setPythonConsoleVisible(visible)
+        slicer.util.setApplicationLogoVisible(visible)
+        keepToolbars = [
+            slicer.util.findChild(slicer.util.mainWindow(), "MainToolBar"),
+            slicer.util.findChild(slicer.util.mainWindow(), "ViewToolBar"),
+            slicer.util.findChild(slicer.util.mainWindow(), "CustomToolBar"),
+        ]
+        slicer.util.setToolbarsVisible(visible, keepToolbars)
 
-  def modifyWindowUI(self):
+    def modifyWindowUI(self):
 
-    # Custom toolbar
-    mainToolBar = slicer.util.findChild(slicer.util.mainWindow(), "MainToolBar")
-    self.CustomToolBar = qt.QToolBar("CustomToolBar")
-    self.CustomToolBar.name = "CustomToolBar"
-    slicer.util.mainWindow().insertToolBar(mainToolBar, self.CustomToolBar)
+        # Custom toolbar
+        mainToolBar = slicer.util.findChild(slicer.util.mainWindow(), "MainToolBar")
+        self.CustomToolBar = qt.QToolBar("CustomToolBar")
+        self.CustomToolBar.name = "CustomToolBar"
+        slicer.util.mainWindow().insertToolBar(mainToolBar, self.CustomToolBar)
 
-    # Settings dialog
-    gearIcon = qt.QIcon(self.resourcePath("Icons/Gears.png"))
-    self.settingsAction = self.CustomToolBar.addAction(gearIcon, "")
-    self.settingsDialog = slicer.util.loadUI(self.resourcePath("UI/Settings.ui"))
-    self.settingsUI = slicer.util.childWidgetVariables(self.settingsDialog)
-    self.settingsUI.CustomUICheckBox.toggled.connect(self.setCustomUIVisible)
-    self.settingsUI.CustomStyleCheckBox.toggled.connect(self.toggleStyle)
-    self.settingsAction.triggered.connect(self.raiseSettings)
+        # Settings dialog
+        gearIcon = qt.QIcon(self.resourcePath("Icons/Gears.png"))
+        self.settingsAction = self.CustomToolBar.addAction(gearIcon, "")
+        self.settingsDialog = slicer.util.loadUI(self.resourcePath("UI/Settings.ui"))
+        self.settingsUI = slicer.util.childWidgetVariables(self.settingsDialog)
+        self.settingsUI.CustomUICheckBox.toggled.connect(self.setCustomUIVisible)
+        self.settingsUI.CustomStyleCheckBox.toggled.connect(self.toggleStyle)
+        self.settingsAction.triggered.connect(self.raiseSettings)
 
-  def toggleStyle(self, visible):
-    if visible:
-      self.applyApplicationStyle()
-    else:
-      slicer.app.styleSheet = ""
+    def toggleStyle(self, visible):
+        if visible:
+            self.applyApplicationStyle()
+        else:
+            slicer.app.styleSheet = ""
 
-  def raiseSettings(self, unused):
-    self.settingsDialog.exec()
+    def raiseSettings(self, unused):
+        self.settingsDialog.exec()
 
-  def setCustomUIVisible(self, visible):
-    self.setSlicerUIVisible(not visible)
+    def setCustomUIVisible(self, visible):
+        self.setSlicerUIVisible(not visible)
 
-  def applyApplicationStyle(self):
-    self.applyStyle([slicer.app], "Home.qss")
+    def applyApplicationStyle(self):
+        self.applyStyle([slicer.app], "Home.qss")
 
-  def applyStyle(self, widgets, styleSheetName):
-    stylesheetfile = self.resourcePath(styleSheetName)
-    with open(stylesheetfile, "r") as fh:
-      style = fh.read()
-      for widget in widgets:
-        widget.styleSheet = style
+    def applyStyle(self, widgets, styleSheetName):
+        stylesheetfile = self.resourcePath(styleSheetName)
+        with open(stylesheetfile, "r") as fh:
+            style = fh.read()
+            for widget in widgets:
+                widget.styleSheet = style
 
 
 class HomeLogic(ScriptedLoadableModuleLogic):
-  """This class should implement all the actual
-  computation done by your module.  The interface
-  should be such that other python code can import
-  this class and make use of the functionality without
-  requiring an instance of the Widget.
-  Uses ScriptedLoadableModuleLogic base class, available at:
-  https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
-  """
-
-  def run(self, inputVolume, outputVolume, imageThreshold, enableScreenshots=0):
+    """This class should implement all the actual
+    computation done by your module.  The interface
+    should be such that other python code can import
+    this class and make use of the functionality without
+    requiring an instance of the Widget.
+    Uses ScriptedLoadableModuleLogic base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
     """
-    Run the actual algorithm
-    """
-    pass
 
-  def setup3DView(self):
-    layoutManager = slicer.app.layoutManager()
-    # layoutManager.setLayout(slicer.vtkMRMLLayoutNode.SlicerLayoutOneUp3DView)
-    # controller = slicer.app.layoutManager().threeDWidget(0).threeDController()
-    # controller.setBlackBackground()
-    # controller.set3DAxisVisible(False)
-    # controller.set3DAxisLabelVisible(False)
-    # controller.setOrientationMarkerType(3)  # Axis marker
-    # controller.setStyleSheet("background-color: #000000")
+    def run(self, inputVolume, outputVolume, imageThreshold, enableScreenshots=0):
+        """
+        Run the actual algorithm
+        """
+        pass
 
-  def setupSliceViewers(self):
-    for name in slicer.app.layoutManager().sliceViewNames():
-        sliceWidget = slicer.app.layoutManager().sliceWidget(name)
-        self.setupSliceViewer(sliceWidget)
+    def setup3DView(self):
+        layoutManager = slicer.app.layoutManager()
+        # layoutManager.setLayout(slicer.vtkMRMLLayoutNode.SlicerLayoutOneUp3DView)
+        # controller = slicer.app.layoutManager().threeDWidget(0).threeDController()
+        # controller.setBlackBackground()
+        # controller.set3DAxisVisible(False)
+        # controller.set3DAxisLabelVisible(False)
+        # controller.setOrientationMarkerType(3)  # Axis marker
+        # controller.setStyleSheet("background-color: #000000")
 
-  def setupSliceViewer(self, sliceWidget):
-    controller = sliceWidget.sliceController()
-    # controller.setStyleSheet("background-color: #000000")
-    # controller.sliceViewLabel = ""
-    # slicer.util.findChild(sliceWidget, "PinButton").visible = False
-    # slicer.util.findChild(sliceWidget, "ViewLabel").visible = False
-    # slicer.util.findChild(sliceWidget, "FitToWindowToolButton").visible = False
-    # slicer.util.findChild(sliceWidget, "SliceOffsetSlider").spinBoxVisible = False
+    def setupSliceViewers(self):
+        for name in slicer.app.layoutManager().sliceViewNames():
+            sliceWidget = slicer.app.layoutManager().sliceWidget(name)
+            self.setupSliceViewer(sliceWidget)
+
+    def setupSliceViewer(self, sliceWidget):
+        controller = sliceWidget.sliceController()
+        # controller.setStyleSheet("background-color: #000000")
+        # controller.sliceViewLabel = ""
+        # slicer.util.findChild(sliceWidget, "PinButton").visible = False
+        # slicer.util.findChild(sliceWidget, "ViewLabel").visible = False
+        # slicer.util.findChild(sliceWidget, "FitToWindowToolButton").visible = False
+        # slicer.util.findChild(sliceWidget, "SliceOffsetSlider").spinBoxVisible = False

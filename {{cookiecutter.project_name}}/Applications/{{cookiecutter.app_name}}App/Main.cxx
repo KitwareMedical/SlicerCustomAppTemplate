a53f0c58dc94a5dab38e87afd35759d1780b6c3c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-07-13 12:33:38 -0400

    COMP: Update Main.cxx to explicitly include configure headers for used macros
    This fixes a regression introduced in 451194ac6 (COMP: Remove obsolete
    version header includes) and apply a fix similar to 8c2143ee9 (COMP: Explicitly
    include version header where associated macros are used)
    
    See https://discourse.slicer.org/t/custom-app-compile-error-slicer-main-project-version-full-undefined/30577
    
    Co-authored-by: Csaba Pinter <pinter.csaba@gmail.com>
    Co-authored-by: Kambiz Darabi <darabi@m-creations.com>

--- {{cookiecutter.project_name}}/Applications/{{cookiecutter.app_name}}App/Main.cxx
+++ {{cookiecutter.project_name}}/Applications/{{cookiecutter.app_name}}App/Main.cxx
@@ -22,6 +22,8 @@
 // Slicer includes
 #include "qSlicerApplication.h"
 #include "qSlicerApplicationHelper.h"
+#include "vtkSlicerConfigure.h" // For Slicer_MAIN_PROJECT_APPLICATION_NAME
+#include "vtkSlicerVersionConfigure.h" // For Slicer_MAIN_PROJECT_VERSION_FULL
 
 namespace
 {

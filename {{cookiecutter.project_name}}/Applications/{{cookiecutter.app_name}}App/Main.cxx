a84a5fcd569f52bd685ef92a0e7b9713b7029d28
James Butler <james.butler@revvity.com>
2025-09-17 10:17:23 -0400

    ENH: Add template support for defining application display name
    Unlike "APPLICATION_NAME", "APPLICATION_DISPLAY_NAME" can contain space characters.
    
    Compatible with https://github.com/Slicer/Slicer/commit/8e30ae816f8723a4b5fe6bd0512e33d85707f1ee.

--- {{cookiecutter.project_name}}/Applications/{{cookiecutter.app_name}}App/Main.cxx
+++ {{cookiecutter.project_name}}/Applications/{{cookiecutter.app_name}}App/Main.cxx
@@ -49,7 +49,7 @@ int SlicerAppMain(int argc, char* argv[])
 
   if (!window.isNull())
     {
-    QString windowTitle = QString("%1 %2").arg(Slicer_MAIN_PROJECT_APPLICATION_NAME).arg(Slicer_MAIN_PROJECT_VERSION_FULL);
+    QString windowTitle = QString("%1 %2").arg(Slicer_MAIN_PROJECT_APPLICATION_DISPLAY_NAME).arg(Slicer_MAIN_PROJECT_VERSION_FULL);
     window->setWindowTitle(windowTitle);
     }
 
